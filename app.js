b99d05092d795747b41cc904bf93ae834dbb2bcc
saikiransrirangam <skiran201@gmail.com>
2025-11-20 23:09:36 +0530

    convert object to json

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
 }, function(err) {
     if (err) {
-        console.log("error!! " + err.stack + " " + process.env)
+        console.log("error!! " + err.stack + " | " + JSON.stringify(process.env))
     } else {
       //  console.log("MongoDB Connection Successful")
     }
